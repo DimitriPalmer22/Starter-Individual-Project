addce677e4dd3fa18adf24c3a6b12c9fa9e9dadb
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2024-01-20 18:22:00 -0500

    Check mess proximity and start cleaning button

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
     // how close the player has to be to a mess before they are able to start cleaning it
-    private const float MESS_CLEAN_PROXIMITY = 5;
+    private const float MessCleanProximity = 1.5f;
     
     private MessScript _currentlyCleaning;
 
@@ -14,7 +15,7 @@ public class PlayerScript : MonoBehaviour
     
     private KeyCode _cleanButton = KeyCode.E;
 
-    private bool IsCleaning => _currentlyCleaning != null;
+    private bool _isCleaning = false;
     
     // Start is called before the first frame update
     void Start()
@@ -26,11 +27,18 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         MovementInput();
+        ProximityCheck();
+        StartCleaningInput();
+        ScrubInput();
     }
 
+    /// <summary>
+    /// Use WASD to move around.
+    /// Disables movement while cleaning.
+    /// </summary>
     private void MovementInput()
     {
-        if (IsCleaning)
+        if (_isCleaning)
             return;
         
         var horizontalMovement = Input.GetAxisRaw("Horizontal");
@@ -40,5 +48,71 @@ public class PlayerScript : MonoBehaviour
 
         transform.position += movementVector * (movementSpeed * Time.deltaTime);
     }
-    
+
+    /// <summary>
+    /// Finds the closest mess to the player.
+    /// Sets the "currentlyCleaning" variable.
+    /// </summary>
+    private void ProximityCheck()
+    {
+        if (_isCleaning)
+        {
+            _currentlyCleaning = null;
+            return;
+        }
+        
+        // Get all mess objects
+        var messObjects = GameObject.FindGameObjectsWithTag("Mess");
+        
+        // If there are no more messes, skip this function
+        if (messObjects.Length <= 0)
+        {
+            _currentlyCleaning = null;
+            return;
+        }
+        
+        // get the closest mess
+        GameObject closestMess = messObjects
+            .OrderBy(n => Vector2.Distance(transform.position, n.transform.position))
+            .First();
+
+        // test to see if it is in range
+        var messDistance = Vector2.Distance(transform.position, closestMess.transform.position);
+        Debug.Log($"PROXIMITY: {messDistance}");
+        if (messDistance > MessCleanProximity)
+        {
+            _currentlyCleaning = null;
+            return;
+        }
+        
+        _currentlyCleaning = closestMess.GetComponent<MessScript>();
+    }
+
+    /// <summary>
+    /// Use E to start cleaning messes.
+    /// This function puts the player into the isCleaning state.
+    /// Skips the function if the player is already cleaning.
+    /// </summary>
+    private void StartCleaningInput()
+    {
+        if (_isCleaning || _currentlyCleaning == null)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _isCleaning = true;
+        }
+    }
+
+    /// <summary>
+    /// Press Q and E rapidly to scrub away the messes.
+    /// This function only runs if the player is cleaning.
+    /// </summary>
+    private void ScrubInput()
+    {
+        if (!_isCleaning || _currentlyCleaning == null)
+            return;
+        
+        
+    }
 }
